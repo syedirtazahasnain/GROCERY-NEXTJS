9ad14d07c7f194ab2b808d9a8af0595d9a9ea5b3
irtaza <arsal.h@yahoo.com>
2025-07-12 00:36:51 +0500

    updating the next configuration and package.json for docker setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '',
+  reactStrictMode: true,
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // Add this
